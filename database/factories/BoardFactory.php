5cfc055f7d2789409572404d388c12a9b923f7a4
Laure-Sagar <slaure354@gmail.com>
2023-03-15 11:23:03 +0545

    Seeder for Board

--- database/factories/BoardFactory.php
+++ database/factories/BoardFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Board;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +10,27 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class BoardFactory extends Factory
 {
+    protected $model = Board::class;
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'description' => $this->faker->sentence,
+            'earliest_start_date' => $this->faker->date(),
+            'earliest_finish_date' => $this->faker->date(),
+            'latest_start_date' => $this->faker->date(),
+            'latest_finish_date' => $this->faker->date(),
+            'task_duration' => $this->faker->randomNumber(),
+            'dependencies_board' => $this->faker->randomElements(),
+            'status' => $this->faker->randomElement(['in progress', 'completed', 'not started']),
+            'project_id' => function () {
+                return \App\Models\Team::factory()->create()->id;
+            }
         ];
     }
 }
---
+++ database/seeders/BoardSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Board;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class BoardSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Board::factory()->count(10)->create();
+    }
+}
