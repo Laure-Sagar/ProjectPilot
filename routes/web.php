daddade56fc8fb0097d4df99b5f8c29868ed4f98
sahil22-hub <sahilbhetwal22@gmail.com>
2023-04-20 00:59:49 +0545

    algorithm

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,79 @@ Route::get('test1', function () {
 Route::get('task_form', function () {
     return view('task_form');
 });
+
+Route::get('algorithm', function () {
+// Define the tasks as an associative array where the key is the task ID and the value is an array with the duration and dependencies
+// Define the tasks as an associative array where the key is the task ID and the value is an array with the duration and dependencies
+$tasks = array(
+    'A' => array('duration' => 6, 'dependencies' => array()),
+    'B' => array('duration' => 4, 'dependencies' => array()),
+    'C' => array('duration' => 3, 'dependencies' => array('A')),
+    'D' => array('duration' => 4, 'dependencies' => array('B')),
+    'E' => array('duration' => 3, 'dependencies' => array('B')),
+    'F' => array('duration' => 10, 'dependencies' => array()),
+    'G' => array('duration' => 3, 'dependencies' => array('E','F')),
+    'H' => array('duration' => 2, 'dependencies' => array('C', 'D')),
+    // 'E' => array('duration' => 6, 'dependencies' => array('D')),
+    // 'F' => array('duration' => 2, 'dependencies' => array('D')),
+);
+function getSuccessorTasks($tasks, $taskName) {
+    $successors = array();
+    foreach ($tasks as $taskId => $task) {
+        if (in_array($taskName, $task['dependencies'])) {
+            // dd($task);
+            $successors[] = $taskId;
+        }
+    }
+    return $successors;
+}
+
+// foreach($task as $taskId => $task){
+
+// }
+// Step 1: Calculate the earliest start and finish times for each task
+$earliestStartTimes = array();
+$earliestFinishTimes = array();
+foreach ($tasks as $taskId => $task) {
+    $earliestStartTimes[$taskId] = 0;
+    $earliestFinishTimes[$taskId] = $task['duration'];
+    foreach ($task['dependencies'] as $dependency) {
+        $earliestStartTimes[$taskId] = max($earliestStartTimes[$taskId], $earliestFinishTimes[$dependency]);
+    }
+    $earliestFinishTimes[$taskId] = $earliestStartTimes[$taskId] + $task['duration'];
+}
+
+// Step 2: Calculate the latest start and finish times for each task
+$latestStartTimes = array();
+$latestFinishTimes = array();
+$endTime = max($earliestFinishTimes);
+foreach (array_reverse($tasks) as $taskId => $task) {
+    $latestFinishTimes[$taskId] = $endTime;
+    $latestStartTimes[$taskId] = $latestFinishTimes[$taskId] - $task['duration'];
+    foreach (getSuccessorTasks($tasks,$taskId) as $dependency) {
+        $latestFinishTimes[$taskId] = min($latestFinishTimes[$taskId], $latestStartTimes[$dependency]);
+    }
+    $latestStartTimes[$taskId] = $latestFinishTimes[$taskId] - $task['duration'];
+}
+// dd($earliestFinishTimes, $latestFinishTimes);
+// Step 3: Identify the critical path
+$criticalPath = array();
+foreach ($tasks as $taskId => $task) {
+    if ($earliestStartTimes[$taskId] === $latestStartTimes[$taskId]) {
+        $criticalPath[] = $taskId;
+    }
+}
+$criticalTime = 0;
+foreach($tasks as $taskId => $task){
+    if (in_array($taskId, $criticalPath)){
+        $criticalTime += $task['duration'];
+    }
+}
+// Print the critical path
+if (count($criticalPath) > 0) {
+    echo "Critical path: " . implode(" -> ", $criticalPath);
+    echo "Shortest Time to Complete Intire Project: $criticalTime ";
+} else {
+    echo "No critical path found";
+}
+});
