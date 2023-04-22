a6830e9ff8d999b7efb9a1cf2d1444296fb4fb7e
Sagar Chhetri <slaure354@gmail.com>
2023-04-22 13:10:58 +0545

    Dashboard Added;

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,51 +1,63 @@
 <x-app-layout>
+    <style>
+        body {
+            overflow-y: scroll !important;
+        }
+    </style>
+    <div class="overflow-y-auto">
+        <link rel="stylesheet" href="/assets/css/card.css">
 
-    <div class="container">
-        <div class="row justify-center">
-            <div class="col-md-10">
-                <div class="bg-white rounded-lg shadow-lg">
-                    <div class="bg-gray-200 rounded-t-lg px-4 py-2">
-                        <h3 class="text-lg font-semibold text-gray-800">{{ __('Projects') }}</h3>
+        <header>
+            <div class="bg-gray-100">
+                <div class="container mx-auto py-8">
+                    <!-- Repeat task card for each task -->
+                    <div class="bg-white rounded-lg shadow-md p-6 mb-6">
+                        <div class="text-lg font-bold mb-4">Summary</div>
+                        <div class="flex justify-between">
+                            <div class="text-gray-600">Critical Path:</div>
+                            <div class="text-gray-800 font-bold">{{implode(" ---> ", $criticalPath)}}</div>
+                        </div>
+                        <div class="flex justify-between">
+                            <div class="text-gray-600">Shotest Time to Complete the project:</div>
+                            <div class="text-gray-800 font-bold">{{$criticalTime}}</div>
+                        </div>
                     </div>
-                    <div class="p-4">
-                        <table class="table-auto w-full">
-                            <thead>
-                                <tr>
-                                    <th class="px-4 py-2">#</th>
-                                    <th class="px-4 py-2">Name</th>
-                                    <th class="px-4 py-2">Description</th>
-                                    <th class="px-4 py-2">Start Date</th>
-                                    <th class="px-4 py-2">End Date</th>
-                                    <th class="px-4 py-2">Status</th>
-                                    <th class="px-4 py-2">Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($projects as $project)
-                                <tr>
-                                    <td class="border px-4 py-2">{{$project->id}}</td>
-                                    <td class="border px-4 py-2">{{$project->name}}</td>
-                                    <td class="border px-4 py-2">{{$project->description}}</td>
-                                    <td class="border px-4 py-2">{{$project->start_date}}</td>
-                                    <td class="border px-4 py-2">{{$project->end_date}}</td>
-                                    <td class="border px-4 py-2">{{$project->status}}</td>
-                                    <td class="border px-4 py-2">
-                                        <a href="{{ route('project.edit', $project->id) }}"
-                                            class="bg-blue-500 hover:bg-blue-700 text-dark font-bold py-1 px-2 rounded mr-2">Edit</a>
-                                        <a href="{{ route('project.destroy', $project->id) }}" <button type="submit"
-                                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">Delete</button>
-                                        </a>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                        <a href="{{ route('project.create') }}"
-                            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4">Create
-                            Project</a>
+                    <h1 class="text-2xl font-bold mb-4">Task List</h1>
+                    <a href="/{{auth()->user()->current_team_id}}/tasks/create"
+                        class="float-right bg-indigo-600 text-white hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500 font-bold py-2 px-4 rounded-md">Add
+                        Task</a>
+                    <div class=" mt-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                        <!-- Example task card -->
+                        @foreach ($tasks_data as $task)
+                        <a href="/task/1/subtasks">
+                            <div class="bg-white rounded-lg shadow-md py-4 px-4">
+                                <div class="flex justify-between items-center mb-4">
+                                    <span class="text-sm text-gray-600"><i>Duration:</i> <span
+                                            class="font-bold">{{$task->duration}} days</span></span>
+                                </div>
+                                <div class="mb-4">
+                                    <h3 class="text-gray-600 font-bold mb-2"><i>Task Name</i></h3>
+                                    <p class="text-gray-800">{{$task->name}}</p>
+                                </div>
+                                <div class="mb-4">
+                                    <h3 class="text-gray-600 font-bold mb-2"><i>Task Description</i></h3>
+                                    <p class="text-gray-800">{{$task->description}}</p>
+                                </div>
+                                <div class="mb-4">
+                                    <h3 class="text-gray-600 font-bold mb-2"><i>Dependencies</i></h3>
+                                    <ul class="list-disc list-inside">
+                                        @forelse(json_decode($task->dependencies) as $dep)
+                                        <li>@php echo App\Models\Task::find($dep)->name; @endphp</li>
+                                        @empty
+                                        <li>No dependencies</li>
+                                        @endforelse
+                                    </ul>
+                                </div>
+                            </div>
+                        </a>
+                        @endforeach
                     </div>
                 </div>
-            </div>
-        </div>
+        </header>
     </div>
 </x-app-layout>
