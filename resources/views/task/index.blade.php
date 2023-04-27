d7d9f572fd6494708a0bded4e8ccde791288307b
Laure-Sagar <slaure354@gmail.com>
2023-04-27 07:53:46 +0545

    Update: Edit

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -50,50 +50,47 @@
                             <div class="text-gray-800 font-bold">{{ $criticalTime }}</div>
                         </div>
                     </div>
-                        <div class="text-center">
-                           
-                            {{$project_id}}
-                            <a href="{{ route('task.create',['project_id' => $project_id]) }}" class="btn btn-primary">Add Task</a>
-                          </div>
-                          
-                    @if (($tasks_data->first()) != null)
-                        <h1 class="text-2xl font-bold mb-4">{{ $tasks_data->first()->project->name }}</h1>
+                    <div class="text-center">
                         <a href="/{{ auth()->user()->current_team_id }}/tasks/create"
                             class="float-right bg-indigo-600 text-white hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500 font-bold py-2 px-4 rounded-md">Add
                             Task</a>
-                        <div class=" mt-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                            <!-- Example task card -->
-                            @foreach ($tasks_data as $task)
-                                <a href="/{{ $task->id }}/subtasks">
-                                    <div class="bg-white rounded-lg shadow-md py-4 px-4">
-                                        <div class="flex justify-between items-center mb-4">
-                                            <span class="text-sm text-gray-600"><i>Duration:</i> <span
-                                                    class="font-bold">{{ $task->duration }} days</span></span>
-                                        </div>
-                                        <div class="mb-4">
-                                            <h3 class="text-gray-600 font-bold mb-2"><i>Task Name</i></h3>
-                                            <p class="text-gray-800">{{ $task->name }}</p>
-                                        </div>
-                                        <div class="mb-4">
-                                            <h3 class="text-gray-600 font-bold mb-2"><i>Task Description</i></h3>
-                                            <p class="text-gray-800">{{ $task->description }}</p>
-                                        </div>
-                                        <div class="mb-4">
-                                            <h3 class="text-gray-600 font-bold mb-2"><i>Dependencies</i></h3>
-                                            <ul class="list-disc list-inside">
-                                                @forelse(json_decode($task->dependencies) as $dep)
-                                                    <li>@php echo App\Models\Task::find($dep)->name; @endphp</li>
-                                                @empty
-                                                    <li>No dependencies</li>
-                                                @endforelse
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </a>
-                            @endforeach
-                        </div>
+                    </div>
+
+                    @if (($tasks_data->first()) != null)
+                    <h1 class="text-2xl font-bold mb-4">{{ $tasks_data->first()->project->name }}</h1>
+                    <div class=" mt-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                        <!-- Example task card -->
+                        @foreach ($tasks_data as $task)
+                        <a href="/{{ $task->id }}/subtasks">
+                            <div class="bg-white rounded-lg shadow-md py-4 px-4">
+                                <div class="flex justify-between items-center mb-4">
+                                    <span class="text-sm text-gray-600"><i>Duration:</i> <span class="font-bold">{{
+                                            $task->duration }} days</span></span>
+                                </div>
+                                <div class="mb-4">
+                                    <h3 class="text-gray-600 font-bold mb-2"><i>Task Name</i></h3>
+                                    <p class="text-gray-800">{{ $task->name }}</p>
+                                </div>
+                                <div class="mb-4">
+                                    <h3 class="text-gray-600 font-bold mb-2"><i>Task Description</i></h3>
+                                    <p class="text-gray-800">{{ $task->description }}</p>
+                                </div>
+                                <div class="mb-4">
+                                    <h3 class="text-gray-600 font-bold mb-2"><i>Dependencies</i></h3>
+                                    <ul class="list-disc list-inside">
+                                        @forelse(json_decode($task->dependencies) as $dep)
+                                        <li>@php echo App\Models\Task::find($dep)->name; @endphp</li>
+                                        @empty
+                                        <li>No dependencies</li>
+                                        @endforelse
+                                    </ul>
+                                </div>
+                            </div>
+                        </a>
+                        @endforeach
+                    </div>
                     @else
-                        <h1>No Tasks</h1>
+                    <h1>No Tasks</h1>
                     @endif
                 </div>
         </header>
